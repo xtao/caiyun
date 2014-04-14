59ba02bde1cee5515a770f15c0752e057e403bbb
XTao <xutao881001@gmail.com>
2014-04-14 23:03:18 +0800

    Add event reply.

--- app.py
+++ app.py
@@ -16,12 +16,17 @@ app.add_url_rule('/', view_func=weixin.view_func)
 CAIYUN_API = app.config['CAIYUN_API']
 CAIYUN_TOKEN = app.config['CAIYUN_TOKEN']
 
+DEFAULT_MSG = u'主页君对您的文字表示理解不能……点击底部输入栏右侧的加号，然后选择位置，主页君才可以为您报天气呢。'
+SUBSCRIBE_MSG = u'感谢您关注彩云天气的微信公众号！' \
+    u'点击底部输入栏右侧的加号，然后选择位置，即可为您预报未来一小时几点几分下雨，现在就试试吧！'
+
 
 @weixin.register('*')
 def reply(**kwargs):
     username = kwargs.get('sender')
     sender = kwargs.get('receiver')
     content = kwargs.get('content')
+    content = DEFAULT_MSG
     type = kwargs.get('type')
     if type == 'location':
         x = kwargs.get('location_x')
@@ -37,7 +42,10 @@ def reply(**kwargs):
         return weixin.reply(username,
                             sender=sender,
                             content=content)
-    content = u"请发送微信位置过来，感谢。"
+    elif type == 'event':
+        event = kwargs.get('event')
+        if event == 'subscribe':
+            content = SUBSCRIBE_MSG
     return weixin.reply(username,
                         sender=sender,
                         content=content)
